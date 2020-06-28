6ad10c4ad173dca15709975f288d744a11ef7c9b
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2020-06-28 12:05:50 +0300

    Added test to resolve by name

--- Example/Tests/Inject/InjectTests.swift
+++ Example/Tests/Inject/InjectTests.swift
@@ -4,15 +4,21 @@ import DICE
 class InjectTests: XCTestCase {
     
     @Inject var injectableService: InjectableServiceType
+    @Inject("injectableService2") var injectableService2: InjectableServiceType
     
     override func setUp() {
         super.setUp()
         
         Dependencies.shared.register({ InjectableService() as InjectableServiceType })
+        Dependencies.shared.register({ InjectableService() as InjectableServiceType }, with: "injectableService2")
     }
     
     func testInjectatbleServiceShouldBeAccessible() {
         injectableService.test()
+    }
+    
+    func testInjectableServiceWithNameShouldBeAccessible() {
+        injectableService2.test()
     }
     
 }
