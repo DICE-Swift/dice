f03b6826ebdea62ffc3eb7148827f1ddb781991c
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2020-09-10 20:42:41 +0300

    Add tests

---
+++ DICETests/SwiftUI/EnvironmentInjectedTests.swift
@@ -0,0 +1,71 @@
+//
+//  EnvironmentInjectedTests.swift
+//  DICETests
+//
+//  Created by nb-058-41b on 9/10/20.
+//  Copyright © 2020 DICE. All rights reserved.
+//
+
+#if canImport(SwiftUI) && canImport(Combine)
+
+import SwiftUI
+import Combine
+import XCTest
+@testable import DICE
+
+class EnvironmentInjectedTests: XCTestCase {
+    
+    let container = DIContainer()
+    
+}
+
+private protocol ServiceType {
+    
+}
+
+private class Service: ServiceType {
+    let barService: BarServiceType
+    
+    init(_ barService: BarServiceType) {
+        self.barService = barService
+    }
+}
+
+private class DummyClass {
+    @EnvironmentInjected var fooService: FooService
+    @EnvironmentInjected("dependency1") var barService: BarService
+    @EnvironmentInjected var service: Service
+}
+
+extension EnvironmentInjectedTests {
+    
+    func testInjectedServicesShouldBeInitializedAndAccessible() {
+        container.register { _ in
+            return FooService()
+        }
+        
+        container.register(tag: "dependency1") { _ in
+            return BarService(test: "stringDependency1")
+        }
+        
+        container.register(BarServiceType.self) { _ in
+            return BarService(test: "internalDependency")
+        }
+        
+        container.register(Service.self) { container in
+            let _: BarService = container.resolve(tag: "dependency1")
+            let barService: BarServiceType = container.resolve()
+            return Service(barService)
+        }
+        
+        DICE.use(container)
+        
+        let dummyClass = DummyClass()
+        XCTAssertNotNil(dummyClass.fooService)
+        XCTAssertNotNil(dummyClass.barService)
+        XCTAssertNotNil(dummyClass.service)
+    }
+    
+}
+
+#endif
---
+++ DICETests/SwiftUI/EnvironmentObvservableInjectedTests.swift
@@ -0,0 +1,67 @@
+//
+//  EnvironmentObvservableInjectedTests.swift
+//  DICETests
+//
+//  Created by nb-058-41b on 9/10/20.
+//  Copyright © 2020 DICE. All rights reserved.
+//
+
+#if canImport(SwiftUI) && canImport(Combine)
+
+import SwiftUI
+import Combine
+import XCTest
+@testable import DICE
+
+class EnvironmentObservableInjectedTests: XCTestCase {
+    
+    let container = DIContainer()
+    
+}
+
+private class ViewModel: ObservableObject {
+    let barService: BarServiceType?
+    
+    init(_ barService: BarServiceType? = nil) {
+        self.barService = barService
+    }
+}
+
+private struct DummyView: View {
+    @EnvironmentObservableInjected var viewModelOne: ViewModel
+    @EnvironmentObservableInjected("dependency1") var viewModelTwo: ViewModel
+    
+    var body: some View {
+        Text("")
+    }
+}
+
+extension EnvironmentObservableInjectedTests {
+    
+    func testInjectedServicesShouldBeInitializedAndAccessible() {
+        container.register { _ in
+            return ViewModel()
+        }
+        
+        container.register(tag: "dependency1") { _ in
+            return ViewModel()
+        }
+        
+        container.register(BarServiceType.self) { _ in
+            return BarService()
+        }
+        container.register(ViewModel.self) { container in
+            let barService: BarServiceType = container.resolve()
+            return ViewModel(barService)
+        }
+        
+        DICE.use(container)
+        
+//        let dummyView = DummyView()
+//        XCTAssertNotNil(dummyView.viewModelOne)
+//        XCTAssertNotNil(dummyView.viewModelTwo)
+    }
+    
+}
+
+#endif
